d4b573073571b79a90f9ad0d80a2d49f3025ba7d
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-04-26 12:25:17 -0500

    server created and database connected

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,19 +9,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/api', apiRoutes);
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database Connected');
+    });
 
+    app.get('/', (req, res) => {
+        res.json({
+            message: 'Hello World'
+        });
+});
 
 //Default response for any other request (not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-db.connect(err => {
-    if (err) throw err;
-    console.log('Database Connected');
-
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-    })
+    
 });
